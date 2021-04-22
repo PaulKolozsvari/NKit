9c34987476995b0422d58df5c816e8850895e352
paulkolo@figlut.com <paulkolo@figlut.com>
2021-04-22 15:00:58 +0200

    Added sort direction and filter model classes to NKit.Core.

---
+++ source/NKit.Core/Data/DB/SQLQuery/SortDirectionCore.cs
@@ -0,0 +1,67 @@
+﻿namespace NKit.Data.DB.SQLQuery
+{
+    #region Using Directives
+
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    #endregion //Using Directives
+
+    public class SortDirectionCore
+    {
+        #region Constants
+
+        /// <summary>
+        /// Sort in an ascending order.
+        /// </summary>
+        public const string ASCENDING = "ASC";
+        /// <summary>
+        /// Sort in a descending order
+        /// </summary>
+        public const string DESCENDING = "DESC";
+
+        #endregion //Constants
+
+        #region Methods
+
+        /// <summary>
+        /// Gets the SortDirectionType from a string representation of the sort directtion.
+        /// </summary>
+        /// <param name="sortDirection"></param>
+        /// <returns></returns>
+        public static SortDirectionTypeCore GetSortDirectionType(string sortDirection)
+        {
+            sortDirection = sortDirection != null ? sortDirection.ToUpper() : string.Empty;
+            switch (sortDirection)
+            {
+                case ASCENDING:
+                    return SortDirectionTypeCore.Ascending;
+                case DESCENDING:
+                    return SortDirectionTypeCore.Descending;
+                default:
+                    return SortDirectionTypeCore.Ascending;
+            }
+        }
+
+        /// <summary>
+        /// Gets the string representation of th sort direction from the SortDirection type.
+        /// </summary>
+        /// <param name="sortDirectionType"></param>
+        /// <returns></returns>
+        public static string GetSortDirection(SortDirectionTypeCore sortDirectionType)
+        {
+            switch (sortDirectionType)
+            {
+                case SortDirectionTypeCore.Ascending:
+                    return ASCENDING;
+                case SortDirectionTypeCore.Descending:
+                    return DESCENDING;
+                default:
+                    return ASCENDING;
+            }
+        }
+
+        #endregion //Methods
+    }
+}
