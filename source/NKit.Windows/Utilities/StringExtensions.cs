6df69974c809e084a5c1ec2759308ee5e2dd82a8
paulkolo@figlut.com <paulkolo@figlut.com>
2022-03-16 13:44:25 +0200

    Added StringExtensions with some extension methods on strings to parse a string to an enum.

---
+++ source/NKit.Windows/Utilities/StringExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace NKit.Utilities
+{
+    #region region name
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    #endregion //region name
+
+    public static class StringExtensions
+    {
+        #region Methods
+
+        /// <summary>
+        /// Parses the string to a specified enum.
+        /// </summary>
+        public static T ParseToEnum<T>(this string enumString)
+        {
+            return (T)Enum.Parse(typeof(T), enumString);
+        }
+
+        /// <summary>
+        /// Parses the string to a specified enum.
+        /// </summary>
+        public static bool TryParseToEnum<TEnum>(this string enumString, out TEnum result) where TEnum : struct
+        {
+            return Enum.TryParse<TEnum>(enumString, out result);
+        }
+
+        #endregion //Methods
+    }
+}
